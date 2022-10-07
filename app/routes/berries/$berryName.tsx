80f79d011f8b786df29e321f7aa79f4ac20b9f1f
yaikohi <erik.beem@hotmail.com>
2022-10-07 15:07:10 +0200

    Add pagination for berries, needs refactor

--- app/routes/berries/$berryName.tsx
+++ app/routes/berries/$berryName.tsx
@@ -1,6 +1,7 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData, useMatches } from "@remix-run/react";
+import type { Berry as BerryType } from "~/../types/berry";
 import { BerryInfo } from "~/components/berry-info";
 import { getBerryByName } from "~/models/berry.server";
 
@@ -12,12 +13,40 @@ export const loader: LoaderFunction = async ({ request, context, params }) => {
 
 export default function Berry() {
   const { berry } = useLoaderData();
-  
+  const relatedRoutes = useMatches();
+
+  const berries = relatedRoutes[1].data?.berries;
+  const currentBerryIndex = relatedRoutes[1].data?.berries.findIndex(
+    (bry: BerryType) => bry.name === berry.name
+  );
+  const nextBerryIndex =
+    currentBerryIndex === berries.length - 1 ? 0 : currentBerryIndex + 1;
+  const prevBerryIndex =
+    currentBerryIndex === 0 ? berries.length - 1 : currentBerryIndex - 1;
+
+  const nextBerryName = berries[nextBerryIndex]?.name;
+  const prevBerryName = berries[prevBerryIndex]?.name;
+
+  const nextBerryRoute = `${relatedRoutes[1].pathname}/${nextBerryName}`;
+  const prevBerryRoute = `${relatedRoutes[1].pathname}/${prevBerryName}`;
+
   return (
     <div className="">
       <BerryInfo berry={berry} />
-      {/* TODO: Add pagination to the next berry */}
-      <Link to="">Next</Link>
+      <div className="flex justify-between">
+        <Link
+          className="p-2 text-white bg-blue-300 rounded-xl"
+          to={prevBerryRoute}
+        >
+          Back ({prevBerryName})
+        </Link>
+        <Link
+          className="p-2 text-white bg-blue-300 rounded-xl"
+          to={nextBerryRoute}
+        >
+          Next ({nextBerryName})
+        </Link>
+      </div>
     </div>
   );
 }
